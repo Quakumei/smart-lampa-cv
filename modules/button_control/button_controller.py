22086ba90a7f1c3d1caf0a70e47d19dea6902826
Fascinat0r <fascinator2904@gmail.com>
2024-01-14 01:58:59 +0300

    Init

--- modules/button_control/button_controller.py
+++ modules/button_control/button_controller.py
@@ -2,18 +2,19 @@ from config.settings import PIN_BUTTON_L, PIN_BUTTON_R
 from modules.button_control.Button import Button
 
 
-def button_l_handler(a):
-    print(f'handler {a}')
+def button_handler(btn, a):
+    print(f'handler {a} get {btn.action()}')
 
 
 def demo():
     button_l = Button(PIN_BUTTON_L)
     button_r = Button(PIN_BUTTON_R)
+    button_l.attach(lambda: button_handler(button_l, "a"))
+    button_r.attach(lambda: button_handler(button_r, "b"))
 
     print("Start")
     while True:
         button_l.tick()
         button_r.tick()
-        a, b = button_l.action(), button_r.action()
-        if a or b:
-            print(a, b)
+
+demo()
