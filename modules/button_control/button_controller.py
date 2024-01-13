67b3e312d80acda4c1992cbf1156ea9ec87e779d
Fascinat0r <fascinator2904@gmail.com>
2024-01-14 02:15:38 +0300

    Done: Click, Clicks, Hold, Hold C!

--- modules/button_control/button_controller.py
+++ modules/button_control/button_controller.py
@@ -5,7 +5,6 @@ from modules.button_control.ButtonStatus import ButtonStatus
 
 def button_handler(btn: Button, a):
     status = btn.action()
-    print(f'Handler {a} get {status}')
     match status:
         case ButtonStatus.EB_PRESS:
             print("Press")
@@ -26,7 +25,7 @@ def button_handler(btn: Button, a):
         case ButtonStatus.EB_REL_STEP:
             print("Release Step")
         case ButtonStatus.EB_REL_STEP_C:
-            print("Release Step C")
+            print(f"Release Step C with {btn.getClicks()} clicks")
         case _:
             print("Unknown status")
 
@@ -34,15 +33,12 @@ def button_handler(btn: Button, a):
 def demo():
     button_l = Button(PIN_BUTTON_L)
     button_r = Button(PIN_BUTTON_R)
-    button_l.attach(button_handler(button_l, 0))
-    button_r.attach(button_handler(button_r, 1))
+    button_l.attach(lambda: button_handler(button_l, 0))
+    button_r.attach(lambda: button_handler(button_r, 1))
     print("Start")
     while True:
         button_l.tick()
         button_r.tick()
-        a, b = button_l.action(), button_r.action()
-        if a or b:
-            print(a, b)
 
 
 if __name__ == "__main__":
