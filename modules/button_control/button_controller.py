356ab7e30ca81f9a2143f219e2e8c6f94aadfe23
Fascinat0r <fascinator2904@gmail.com>
2024-01-14 02:56:35 +0300

    Fix button_handler *when one button was triggered, it processed all

--- modules/button_control/button_controller.py
+++ modules/button_control/button_controller.py
@@ -3,41 +3,39 @@ from modules.button_control.Button import Button
 from modules.button_control.ButtonStatus import ButtonStatus
 
 
-def button_handler(buttons: tuple):
-    for idx, btn in enumerate(buttons):
-        status = btn.action()
-        print(f'Handler {idx} get {status}')
-        match status:
-            case ButtonStatus.EB_PRESS:
-                print("Press")
-            case ButtonStatus.EB_HOLD:
-                print("Hold")
-            case ButtonStatus.EB_STEP:
-                print("Step")
-            case ButtonStatus.EB_RELEASE:
-                print("Release")
-            case ButtonStatus.EB_CLICK:
-                print("Click")
-            case ButtonStatus.EB_CLICKS:
-                print(f"Clicks: {btn.getClicks()} times")
-            case ButtonStatus.EB_REL_HOLD:
-                print("Release Hold")
-            case ButtonStatus.EB_REL_HOLD_C:
-                print("Release Hold C")
-            case ButtonStatus.EB_REL_STEP:
-                print("Release Step")
-            case ButtonStatus.EB_REL_STEP_C:
-                print(f"Release Step C with {btn.getClicks()} clicks")
-            case _:
-                print("Unknown status")
+def button_handler(btn, buttons: tuple):
+    status = btn.action()
+    match status:
+        case ButtonStatus.EB_PRESS:
+            print("Press")
+        case ButtonStatus.EB_HOLD:
+            print("Hold")
+        case ButtonStatus.EB_STEP:
+            print("Step")
+        case ButtonStatus.EB_RELEASE:
+            print("Release")
+        case ButtonStatus.EB_CLICK:
+            print("Click")
+        case ButtonStatus.EB_CLICKS:
+            print(f"Clicks: {btn.getClicks()} times")
+        case ButtonStatus.EB_REL_HOLD:
+            print("Release Hold")
+        case ButtonStatus.EB_REL_HOLD_C:
+            print("Release Hold C")
+        case ButtonStatus.EB_REL_STEP:
+            print("Release Step")
+        case ButtonStatus.EB_REL_STEP_C:
+            print(f"Release Step C with {btn.getClicks()} clicks")
+        case _:
+            pass
 
 
 def demo():
     button_l = Button(PIN_BUTTON_L)
     button_r = Button(PIN_BUTTON_R)
     buttons = (button_l, button_r)
-    button_l.attach(lambda: button_handler(buttons))
-    button_r.attach(lambda: button_handler(buttons))
+    button_l.attach(lambda: button_handler(button_l, buttons))
+    button_r.attach(lambda: button_handler(button_r, buttons))
     print("Start")
     while True:
         button_l.tick()
