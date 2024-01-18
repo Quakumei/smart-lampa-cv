e5c1b31bb8f181e076ab8ba6af398b4e3a3bf229
Сергей Головатюков <2003gsa20152@gmail.com>
2024-01-18 20:03:48 +0300

    поправил

--- modules/movement_control/rapprochement.py
+++ modules/movement_control/rapprochement.py
@@ -1,40 +1,34 @@
+import time
 import numpy
 
-def define_new_camera_pos(
-        cam_x,     # положение нижнего сервомотора (вращающегося по оси х)
-        cam_y,     # положение верхнего сервомотора (вращающегося по оси у)
-        cam_x_a,   # ускорение нижнего сервомотора
+def define_catch_pos(
+        servo1,    # нижний сервомотор
+        servo2,    # верхний сервомотор
         cur_x,     # координата х руки
         cur_y,     # координата у руки
-        cur_t,     # момент времени
-        cam_x_v=0, # скорость нижнего сервомотора
-        prev_x=0,  # предыдущая координата х руки
-        prev_y=0,  # предыдущая координата у руки
-        prev_t=0   # предыдущий момент времени
+        prev_x,  # предыдущая координата х руки
+        prev_y,  # предыдущая координата у руки
     ):
+    cur_t = int(time.time())
+    prev_t = servo1.previous_millis
+
     target_x_v = (cur_x - prev_x) / (cur_t - prev_t) # скорость руки по оси х
     target_y_v = (cur_y - prev_y) / (cur_t - prev_t) # скорость руки по оси y
 
     # уравнение для нахождения момента времени пересечения камеры и руки
     eq_roots =\
     numpy.roots([
-        cam_x_a / 2,
-        cam_x_v - target_x_v,
-        target_x_v * prev_t + cam_x - prev_x
+        (servo1.acceleration - servo2.acceleration) / 2,
+        (cur_y - prev_y - cur_x + prev_x) / (cur_t - prev_t)
+            + servo1.current_speed - servo2.current_speed,
+        prev_t / (cur_t - prev_t) * (cur_x - prev_x - cur_y + prev_y)
+            + servo1.current_pos - servo2.current_pos - prev_x + prev_y
     ])
     # выбираем первый неотрицательный корень
     catch_t = next((root for root in eq_roots if root >= 0), 0)
 
     # определяем точку перехвата
-    catch_x = prev_x + target_x_v * catch_t
-    catch_y = prev_y + target_y_v * catch_t
+    catch_x = cur_x + target_x_v * (catch_t - cur_t)
+    catch_y = cur_y + target_y_v * (catch_t - cur_t)
 
-    # вычисляем отношение расстояния, пройденного рукой, к расстоянию до точки перехвата
-    x_coeff = abs((cur_x - prev_x) / (catch_x - prev_x))
-    y_coeff = abs((cur_y - prev_y) / (catch_y - prev_y))
-
-    # определяем точку, в которой должна оказаться камера
-    cam_new_x = cam_x + (catch_x - cam_x) * x_coeff
-    cam_new_y = cam_y + (catch_y - cam_y) * y_coeff
-
-    return (cam_new_x, cam_new_y)
+    return (catch_x, catch_y)
